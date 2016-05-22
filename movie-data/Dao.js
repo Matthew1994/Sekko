25070c48800d5d1681ddf76d220897be0a4c3769
Matthew1994 <wb-lmh190042@alibaba-inc.com>
2016-05-22 17:32:13 +0800

    完成movie dao

--- movie-data/Dao.js
+++ movie-data/Dao.js
@@ -84,33 +84,145 @@ var movie = sequelize.define('movie', {
 });
 
 
-//创建表
-/*
-movie.sync({
-    force: false
-}).error(function(err, result) {
-	console.error('[ ERROR - ] MOVIE TABLE BUILD ERROR');
-	console.error(err);
-	console.error(result);
-}).done(function() {
-	console.log('[ DONE - ] MOVIE TABLE BUILD');
-});
-*/
-
 module.exports = function() {
+
+    //创建表
+    movie.sync({
+        force: false
+    }).error(function(err, result) {
+        console.error('[ ERROR - ] MOVIE TABLE BUILD ERROR');
+        console.error(err);
+        console.error(result);
+    }).done(function() {
+        console.log('[ DONE - ] MOVIE TABLE BUILD');
+    });
+
+    /*
+     * @param record 为符合已定义orm对象格式的对象
+     * @param callback 回调函数，需要传入两个参数，第一个参数表示是否插入成功，第二参数表示成功的返回信息/失败信息
+     */
     this.create = function(record, callback) {
         movie.create(record)
             .then(function(result) {
                 console.log('[SUCCESS -] INSERTED AN RECORD INTO MOVIE ' + record['name']);
                 if (!!callback)
-                	callback(result);
+                    callback(true, result);
             })
             .catch(function(err) {
                 console.log('[ERROR -] FAILED TO INSERT AN RECORD INTO MOVIE') + record['name'];
                 console.log(err);
+                callback(false, err);
             });
     };
-    this.retrieve = function() {
-    	
+
+    /*
+     * @param condition 查找条件，为key-value对象
+     * @param callback 回调函数，需要传入两个参数，第一个参数表示是否能查找到，第二参数表示查找的数据
+     */
+    this.findOne = function(condition, callback) {
+        movie.findOne({ where: condition }).then(function(result) {
+            if (!result) {
+                callback(false, null);
+            } else {
+                callback(true, result['dataValues']);
+            }
+            console.log('[SUCCESS -] FOUND AN RECORD FROM MOVIE WHERE ' + log(condition));
+        }).catch(function(err) {
+            console.log('[ERROR -] FAILED TO FIND AN RECORD FROM MOVIE WHERE' + log(condition));
+            console.log(err);
+        });
     }
+
+    /*
+     * @param condition 查找条件，为key-value对象
+     * @param callback 回调函数，需要传入两个参数，第一个参数表示是否能查找到，第二参数表示查找的数据的数组
+     * @param limit  返回条数，默认情况下返回全部
+     */
+    this.find = function(condition, callback, limit) {
+        movie.findAndCountAll({ where: condition, limit: limit })
+            .then(function(result) {
+                if (!result.count) {
+                    callback(0, []);
+                    return;
+                }
+                var movies = [];
+                result.rows.forEach(function(ele) {
+                    movies.push(ele['dataValues']);
+                });
+                console.log('[SUCCESS -] FOUND ALL RECORDS FROM MOVIE WHERE ' + log(condition));
+                callback(result.count, movies);
+            }).catch(function(err) {
+                console.log('[ERROR -] FAILED TO FIND ALL RECORD FROM MOVIE WHERE ') + log(condition);
+                console.log(err);
+            });
+    };
+
+    /*
+     * @param condition 查找条件，为key-value对象
+     * @param callback 回调函数， 传入参数为删除的记录的条数
+     */
+    this.delete = function(condition, callback) {
+        movie.findAll({ where: condition })
+            .then(function(movies) {
+                if (movies.length === 0) {
+                    callback(0);
+                    console.log('[SUCCESS -] DELETED 0 RECORD FROM MOVIE WHERE ' + log(condition));
+                    return;
+                } else {
+                    var len = movies.length;
+                    movies.forEach(function(ele, index) {
+                        var name = ele['dataValues']['name'];
+                        ele.destroy().then(function(info) {
+                            console.log('[SUCCESS -] DELETED AN RECORD FROM MOVIE - ' + name);
+                            if (index == len - 1)
+                                callback(len);
+                        });
+                    });
+                    console.log('[SUCCESS -] DELETED ' + len + ' RECORD FROM MOVIE WHERE ' + log(condition));
+                }
+            })
+            .catch(function(err) {
+                console.log('[ERROR -] FAILED TO DELETE RECORD FROM MOVIE WHERE ') + log(condition);
+                console.log(err);
+            });
+    };
+
+    /*
+     * @param condition 查找条件，为key-value对象
+     * @param callback 回调函数， 传入参数为修改的记录的条数
+     */
+    this.update = function(condition, newProp, callback) {
+        movie.findAll({ where: condition })
+            .then(function(movies) {
+                if (movies.length === 0) {
+                    callback(0);
+                    console.log('[SUCCESS -] UPDATE 0 RECORD FROM MOVIE WHERE ' + log(condition));
+                    return;
+                } else {
+                    var len = movies.length;
+                    movies.forEach(function(ele, index) {
+                        var name = ele['dataValues']['name'];
+                        ele.update(newProp).then(function(info) {
+                            console.log('[SUCCESS -] UPDATE AN RECORD FROM MOVIE - ' + name);
+                            console.log('[SUCCESS -] UPDATE RESULT - ' + log(newProp));
+                            if (index == len - 1)
+                                callback(len);
+                        });
+                    });
+                    console.log('[SUCCESS -] UPDATE ' + len + ' RECORD FROM MOVIE WHERE ' + log(condition));
+                }
+            })
+            .catch(function(err) {
+                console.log('[ERROR -] FAILED TO UPDATE ALL RECORD FROM MOVIE WHERE ') + log(condition);
+                console.log(err);
+            });
+    }
+}
+
+
+function log(condition) {
+    var info = '';
+    for (var i in condition)
+        info += i.toString() + ' = ' + condition[i].toString() + ', ';
+    return info;
 }
--- movie-data/movie.js
+++ movie-data/movie.js
@@ -1,34 +1,6 @@
 var cheerio = require('cheerio'),
     request = require('request');
 
-var hotMovieIndexUrl = ["http://film.spider.com.cn/guangzh-film----/",
-				  "http://film.spider.com.cn/guangzh-film-----2/",
-				  "http://film.spider.com.cn/guangzh-film-----3/",
-				  "http://film.spider.com.cn/guangzh-film-----4/"
-				 ];
-
-var movieTypeUrl = {
-	'爱情': 'http://film.spider.com.cn/guangzh-film--aq--/',
-	'剧情': 'http://film.spider.com.cn/guangzh-film--jq--/',
-	'冒险': 'http://film.spider.com.cn/guangzh-film--mx--/',
-	'奇幻': 'http://film.spider.com.cn/guangzh-film--qh--/',
-	'动作': 'http://film.spider.com.cn/guangzh-film--dz--/',
-	'犯罪': 'http://film.spider.com.cn/guangzh-film--fz--/',
-	'惊悚': 'http://film.spider.com.cn/guangzh-film--js--/',
-	'家庭': 'http://film.spider.com.cn/guangzh-film--jt--/',
-	'动画': 'http://film.spider.com.cn/guangzh-film--dh--/',
-	'喜剧': 'http://film.spider.com.cn/guangzh-film--xj--/',
-	'科幻': 'http://film.spider.com.cn/guangzh-film--kh--/',
-	'恐怖': 'http://film.spider.com.cn/guangzh-film--kb--/',
-};
-
-var option = {
-	url:movieTypeUrl['恐怖'],
-	method: 'GET',
-	//headers:[{}]
-};
-
-
 module.exports = function() {
 	this.data = {
 		state:false,
