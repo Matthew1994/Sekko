51925114447c152f0993e4f9dc0f521ef6d364de
Matthew1994 <wb-lmh190042@alibaba-inc.com>
2016-05-28 17:45:55 +0800

    完成用户管理部分

---
+++ Sekko/src/main/java/com/sekko/user/UserController.java
@@ -0,0 +1,41 @@
+package com.sekko.user;
+
+import javax.validation.Valid;
+import javax.websocket.server.PathParam;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	@Autowired
+	UserService userService;
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	@PreAuthorize("hasAnyAuthority({'admin', 'user'})")
+	public User getUserById(@PathParam("id") long id) {
+		return userService.getUserById(id);
+	}
+	
+	@RequestMapping(value={"", "/"}, method = RequestMethod.GET)
+	@PreAuthorize("hasAuthority('admin')")
+	public Iterable<User> getAllUsers() {
+		return userService.listUsers();
+	}
+	
+	@RequestMapping(value={"", "/"}, method=RequestMethod.POST)
+	@ResponseStatus(value = HttpStatus.CREATED)
+	@PreAuthorize("permitAll()")
+	public User createUser(@Valid @RequestBody User user) {
+		return userService.createUser(user);
+	}
+	
+}
